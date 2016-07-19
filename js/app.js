b604a5ced7aa24258886391b572af242c6339ac3
FreewheelMaps <freewheel.carto@gmail.com>
2016-07-19 10:46:44 -0400

    fixed https leaflet issue, added sidebar stuff

--- js/app.js
+++ js/app.js
@@ -80,7 +80,7 @@ function animateSidebar() {
 //ZOOMS TO THE POINT WHEN CLICKED FROM THE SIDEBAR
 function sidebarClick(id) {
     $('.feature-row').on('click', function() {
-        map.flyTo([$(this).attr("lat"), $(this).attr("lng")], 15);
+        map.flyTo([$(this).attr("lat"), $(this).attr("lng")], 17);
         /* Hide sidebar and go to the map on small screens */
         if (document.body.clientWidth <= 767) {
             $("#sidebar").hide();
@@ -90,23 +90,19 @@ function sidebarClick(id) {
     });
 }
 
-function syncSidebar(stream) {
-    /* Empty sidebar features */
+function syncSidebar(field, filter) {
+    // Empty sidebar features 
     $("#feature-list tbody").empty();
+    // Add features to side bar
     accessLayer.eachFeature(function(layer) {
-        if (layer.feature.properties.streamName === stream) {
+        if (layer.feature.properties[field] === filter) {
             $("#feature-list tbody").append('<tr class="feature-row" id="' + layer.feature.properties.pointID + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="20" height="20" src="icons/' + getType(layer.feature.properties.pointType)[1] + '"></td><td class="point-name">' + layer.feature.properties.pointName + '</td><td class="stream-mile">' + layer.feature.properties.streamMile + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-        }   
-    });    
-}
-
-function syncList(){
-        /* Update list.js featureList */
+        }
+    });
+    $("#stream-title").html(filter);
+    /* Update list.js featureList */
     featureList = new List("features", {
         valueNames: ["point-name"]
-    });
-    featureList.sort("stream-mile", {
-        order: "desc"
     });
 }
 
@@ -127,11 +123,38 @@ var pTypes = {
     7: ["Dam - No Portage", "damn.svg", "damn.svg"],
     8: ["Confluence", "conf.svg"],
     9: ["Island", "island.svg"],
-    10: ["Reservoir", "lake.svg"]
+    10: ["Reservoir", "lake.svg"],
+    11: ["Stream Gage", "gage.svg"]
 };
+
+var rSide = {
+    1: ["North"],
+    2: ["East"],
+    3: ["South"],
+    4: ["West"],
+    5: ["Center/Island"],
+    6: ["Either"],
+    7: ["n/a"]
+}
 
 function getType(val) {
     return pTypes[val];
+}
+function getSide(val){
+//accounting for nulls right now, but all data will have  a value in future, so we can delete this. 
+    if (val != null){
+        return rSide[val]
+    } else{
+        return "n/a"
+    }
+}
+
+function getAmenities(property){
+    if (property != null){
+        return property
+    } else {
+        return ""
+    }
 }
 
 function getUrl(property) {
@@ -145,6 +168,7 @@ function getUrl(property) {
 
 //GET THE UNIQUE NAMES OF RIVERS AND POPULATE A DROPDOWN IN THE 'FIND A RIVER' MODAL
 var riverNames = []
+var trailNames = []
 
 map = L.map("map", {
     zoom: 8,
@@ -155,8 +179,11 @@ map = L.map("map", {
 
 basemap.addTo(map);
 
+var fullWhere = "pointType IN (1,2,11)"
+
 var accessLayer = L.esri.featureLayer({
     url: access,
+    where: fullWhere,
     pointToLayer: function(geojson, latlng) {
         return L.marker(latlng, {
             icon: L.icon({
@@ -169,14 +196,34 @@ var accessLayer = L.esri.featureLayer({
     onEachFeature: function(feature, layer) {
         if ($.inArray(feature.properties.streamName, riverNames) === -1) {
             riverNames.push(feature.properties.streamName);
-        };
+        }
+        if ($.inArray(feature.properties.waterTrail_Name, trailNames) === -1) {
+            trailNames.push(feature.properties.waterTrail_Name)
+        }
         if (feature.properties) {
             //THIS IS WHAT GOES IN THE MODAL    
-            var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><td colspan='2'>" + feature.properties.streamName + "</td></tr>" + "<tr><td colspan='2'>" + feature.properties.pointDesc + "</td></tr>" + "<tr><td colspan='2'>" + feature.properties.amenities + "<tr><td colspan='2'>" + getUrl(feature.properties.linkURL) + "</td></tr>" + "<tr><th>Stream Mile</th><td>" + feature.properties.streamMile + "</td></tr>" + "<tr><th>Coordinates</th><td>" + feature.properties.lat + ", " + feature.properties.long + "</td></tr>" + "<table>";
+            var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><td colspan='2'>" + feature.properties.streamName + "</td></tr>" + "<tr><td colspan='2'>" + feature.properties.pointDesc + "</td></tr>" + "<tr><td colspan='2'>" + getAmenities(feature.properties.amenities) + "</td></tr>" + "<tr><td colspan='2'>" + getUrl(feature.properties.linkURL) + "</td></tr>" + "<tr><th>Stream Mile</th><td>" + feature.properties.streamMile + "</td></tr>" + "<tr><th>Side of River</th><td>" + getSide(feature.properties.riverSide)[0] + "</td></tr>" + "<tr><th>Coordinates</th><td>" + feature.properties.lat + ", " + feature.properties.long + "</td></tr>" + "<table>";
+
             layer.on({
                 click: function(e) {
                     $("#feature-title").html(feature.properties.pointName + '&nbsp;&nbsp<img width="30" height="30" src="icons/' + getType(layer.feature.properties.pointType)[1] + '">');
-                    $("#feature-info").html(content);
+                    if (feature.properties.pointType == 11) {
+                        //GET USGS GAGE DATA
+                        var gageURL = "http://waterservices.usgs.gov/nwis/iv/?format=json,1.1&sites=" + feature.properties.pointID + "&parameterCd=00060,00065&siteStatus=active"
+
+                        $.getJSON(gageURL, function(data) {
+                            var cfs = data.value.timeSeries[0].values[0].value[0].value
+                            var stage = data.value.timeSeries[1].values[0].value[0].value
+
+                            var gageContent = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><td colspan='2'>Stream Gage on the " + feature.properties.streamName + "</td></tr>" + "<tr><th>Stage (feet)</th><td>" + stage + "</td></tr>" + "<tr><th>Discharge (cfs)</th><td>" + cfs + "</td></tr>" + "<tr><td colspan='2'>" + getUrl(feature.properties.linkURL) + " from USGS</td></tr>" + "<tr><td colspan='2'><img class='hydrograph' src='http://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=" + feature.properties.pointID + "&parm_cd=00060&period=7'/></td></tr>" + "<table>"
+
+                            $("#feature-info").html(gageContent);
+                        });
+
+                    } else {
+                        $("#feature-info").html(content);
+                    }
+
                     $("#featureModal").modal("show");
                     //highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));
                 }
@@ -277,26 +324,75 @@ $("#featureModal").on("hidden.bs.modal", function(e) {
     $(document).on("mouseout", ".feature-row", clearHighlight);
 });
 
-//ONCE ALL DATA LOADS, GET ALL THE RIVER NAMES AND ADD THEM TO THE DROPDOWN LIST IN THE MODAL. 
+//ONCE ALL DATA LOADS, GET ALL THE RIVER and TRAIL NAMES AND ADD THEM TO THE DROPDOWN LISTS IN THE MODALS. 
 accessLayer.on("load", function() {
     for (var i = 0; i < riverNames.length; i++) {
         val = riverNames.sort()[i];
         $("#stream-names").append('<option value="' + val + '">' + val + '</option>');
     }
+    for (var i = 0; i < trailNames.length; i++) {
+        val = trailNames.sort()[i];
+        $("#trail-names").append('<option value="' + val + '">' + val + '</option>');
+    }
+    accessLayer.off("load");
 });
 
-//GET STREAM NAME FROM OPTION, RUN ESRI QUERY, ZOOM TO THOSE FEATURES - still working on the zoom to part
-$("#getStream").click(function() {
-    var stream = $("#stream-names").val();
-    accessLayer.setWhere("streamName ='" + stream + "'", function() {
-        syncSidebar(stream);
-        syncList();
+//GET STREAM NAME FROM OPTION, RUN ESRI QUERY, ZOOM TO THOSE FEATURES
+$(".filter-btn").click(function(evt) {
+    if (this.id === "getStream") {
+        var field = "streamName"
+        var filter = $("#stream-names").val();
+    } else if (this.id == "getTrail") {
+        var field = "waterTrail_Name"
+        var filter = $("#trail-names").val();
+    }
+
+    var expression = field + "='" + filter + "'"
+
+    $("#initial").hide();
+    $("#searchBox").show();
+    
+    accessLayer.setWhere(expression, function() {
+        syncSidebar(field, filter);
     });
+    accessLayer.query()
+        .where(expression)
+        .bounds(function(error, latlngbounds) {
+            map.flyToBounds(latlngbounds);
+        });
+    
 });
 
+//CLEAR WHERE STATEMENTS AND SHOW ALL RIVERS, ZOOM TO FULL STATE VIEW
+$(".view-all").click(function() {
+    accessLayer.setWhere(fullWhere);
+    accessLayer.query().bounds(function(error, latlngbounds) {
+        map.flyToBounds(latlngbounds);
+    });
+    //NEED TO PUT THE DEFAULT STUFF BACK IN THE PANEL AND CLEAR THE LIST OF ITEMS
+    $("#feature-list tbody").empty();
+    $("#stream-names").val("Select a river name...");
+    $("#trail-names").val("Select a trail name...");
+    $("#stream-title").html("Paddling the Palmetto State");
+    $("#initial").show();
+    $("#searchBox").hide();
+});
 
+// Leaflet patch to make layer control scrollable on touch browsers
+var container = $(".leaflet-control-layers")[0];
+if (!L.Browser.touch) {
+    L.DomEvent
+        .disableClickPropagation(container)
+        .disableScrollPropagation(container);
+} else {
+    L.DomEvent.disableClickPropagation(container);
+}
+
+
+
+//NOT SURE IF I AM GOING TO INCLUDE ANY OF THIS OR NOT
 // Typeahead search functionality
-$(document).on("ajaxStop", function() {
+/*$(document).on("ajaxStop", function() {
     $("#loading").hide();
     sizeLayerControl();
     // Fit map to boroughs bounds 
@@ -363,14 +459,4 @@ $(document).on("ajaxStop", function() {
     });
     $(".twitter-typeahead").css("position", "static");
     $(".twitter-typeahead").css("display", "block");
-});
-
-// Leaflet patch to make layer control scrollable on touch browsers
-var container = $(".leaflet-control-layers")[0];
-if (!L.Browser.touch) {
-    L.DomEvent
-        .disableClickPropagation(container)
-        .disableScrollPropagation(container);
-} else {
-    L.DomEvent.disableClickPropagation(container);
-}
+});*/
